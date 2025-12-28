9c590cdf529c90ab529065ea826a8c0dfd44954a
Ewan Miller <ewan.miller@kcl.ac.uk>
2025-12-28 01:13:18 +0000

    move most functionality to base SpinModel2D and fix a number of bugs in the calculations

--- src/LQCD_Engine/SpinModel.cpp
+++ src/LQCD_Engine/SpinModel.cpp
@@ -1,4 +1,54 @@
 #include "SpinModel.h"
+#include "omp.h"
+
+FLOAT SpinModelBase2D::getAction(Lattice2D<FLOAT> &latticeToCheck) {
+
+    FLOAT accum = 0.0;
+
+    #pragma omp parallel for reduction(+:accum)
+    for (uint x = 0; x < latticeToCheck.getSizeX(); x ++){
+        
+        FLOAT accum_thread = 0.0;
+
+        for (uint y = 0; y < latticeToCheck.getSizeY(); y ++){
+
+            if (x == 0) {
+                if (periodicBC) {
+                    accum_thread += getAction(latticeToCheck.getSite(x, 0), latticeToCheck.getSite(x, latticeToCheck.getSizeY() -1));
+                }
+            }
+            else {
+                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x - 1, y));
+            }
+            if (y == 0) {
+                if (periodicBC) {
+                    accum_thread += getAction(latticeToCheck.getSite(0, y), latticeToCheck.getSite(latticeToCheck.getSizeX() -1, y));
+                }
+            }
+            else {
+                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x, y - 1));
+            }
+            if (x == latticeToCheck.getSizeX() -1) {
+                if (periodicBC) {
+                    accum_thread += getAction(latticeToCheck.getSite(x, latticeToCheck.getSizeY() -1), latticeToCheck.getSite(x, 0));
+                }
+            }
+            else {
+                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x + 1, y));
+            }
+            if (y == latticeToCheck.getSizeY() -1) {
+                accum_thread += getAction(latticeToCheck.getSite(latticeToCheck.getSizeX() -1, y), latticeToCheck.getSite(0, y));
+            }
+            else {
+                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x, y + 1));
+            }
+        }
+
+        accum += accum_thread;
+    }
+
+    return accum;
+}
 
 MagneticSpinModel::MagneticSpinModel(int nDims, int sizeX, int sizeY, FLOAT magneticCoupling)
     :
@@ -15,16 +65,19 @@ MagneticSpinModel::MagneticSpinModel(int nDims, int sizeX, int sizeY, FLOAT magn
 }
 
 void MagneticSpinModel::proposeLattice(FLOAT stepSize){
+
+    proposedLattice.set(getCurrentLattice());
+    
     for(int x = 0; x < proposedLattice.getSizeX(); x++){
         for (int y= 0; y < proposedLattice.getSizeY(); y++){
 
             float norm = 0.0;
             // propose some random variations of the spin vectors
             for (int d=0; d < proposedLattice.getNDims(); d++){
-                FLOAT val = proposedLattice.getSite(x,y)[d] + distribution(randomGenerator) *stepSize;
+                FLOAT val = proposedLattice.getSite(x,y)[d] + distribution(randomGenerator) * stepSize;
                 proposedLattice.setSite(x, y, d, val);
 
-                norm += val *val;
+                norm += val * val;
             }
 
             // normalise the spin vectors
@@ -37,45 +90,13 @@ void MagneticSpinModel::proposeLattice(FLOAT stepSize){
     }
 }
 
-FLOAT MagneticSpinModel::getMagneticAction(std::vector<FLOAT> s1, std::vector<FLOAT> s2) {
+FLOAT MagneticSpinModel::getAction(std::vector<FLOAT> s1, std::vector<FLOAT> s2) {
     assert(s1.size() == s2.size());
     FLOAT accum = 0.0;
 
-    for(int d = 0; d < s1.size(); d++){
-        accum -= s1[d] * s2[d];
+    for(uint d = 0; d < s1.size(); d++){
+        accum += s1[d] * s2[d];
     }
 
-    return accum;
-}
-
-FLOAT MagneticSpinModel::getMagneticAction(Lattice2D<FLOAT> &latticeToCheck) {
-    FLOAT accum = 0.0;
-
-    // #### First the sites at the edges of the lattice ######
-    for (int x = 0; x < latticeToCheck.getSizeX(); x ++){
-        if (periodicBC){
-            accum += 2.0 *getMagneticAction(latticeToCheck.getSite(x, 0), latticeToCheck.getSite(x, latticeToCheck.getSizeY() -1));
-        }
-        accum += getMagneticAction(latticeToCheck.getSite(x, 0), latticeToCheck.getSite(x, 1));
-        accum += getMagneticAction(latticeToCheck.getSite(x, latticeToCheck.getSizeY() -1 ), latticeToCheck.getSite(x, latticeToCheck.getSizeY() -2));
-    }
-    for (int y = 0; y < latticeToCheck.getSizeY(); y ++){
-        if (periodicBC){
-            accum += 2.0 *getMagneticAction(latticeToCheck.getSite(0, y), latticeToCheck.getSite(latticeToCheck.getSizeX() -1, y));
-        }
-        accum += getMagneticAction(latticeToCheck.getSite(0, y), latticeToCheck.getSite(1, y));
-        accum += getMagneticAction(latticeToCheck.getSite(latticeToCheck.getSizeX() - 1, y), latticeToCheck.getSite(latticeToCheck.getSizeX() -2, y));
-    }
-
-    // Now do the bulk
-    for (int x = 1; x < latticeToCheck.getSizeX() - 1; x ++){
-        for (int y = 1; y < latticeToCheck.getSizeY() - 1; y ++){
-            accum += getMagneticAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x, y - 1));
-            accum += getMagneticAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x, y + 1));
-            accum += getMagneticAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x - 1, y));
-            accum += getMagneticAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x + 1, y));
-        }
-    }
-
-    return accum;
+    return magneticCoupling * accum;
 }
--- src/LQCD_Engine/SpinModel.h
+++ src/LQCD_Engine/SpinModel.h
@@ -8,11 +8,11 @@ public:
 
     /// @brief Get the action for the current state of the model
     /// @return The action
-    virtual FLOAT getCurrentAction() = 0;
+    inline FLOAT getCurrentAction()  { return getAction(lattice); }
     
     /// @brief Get the action for the proposed state of the model
     /// @return The action
-    virtual FLOAT getProposedAction() = 0;
+    inline FLOAT getProposedAction() { return getAction(proposedLattice); }
 
     /// @brief Propose a new configuration for the lattice
     /// \todo add ability to set the type of proposal using some kinda other object ("Proposer", "PropGenerator"... something like that)
@@ -24,6 +24,12 @@ public:
     inline const Lattice2D<FLOAT> &getProposedLattice() { return proposedLattice; }
     /// @}
 
+    /// @brief Set whether or not to use periodic boundary conditions
+    /// @param newVal The new value
+    inline void setPeriodicBC(bool newVal = true) { periodicBC = newVal; }
+
+    /// @brief set the current lattice values to the proposed ones
+    inline void acceptProposedLattice() { lattice.set(proposedLattice); }
 
 protected:
     Lattice2D<FLOAT> lattice; /// !< The current lattice for this model
@@ -32,6 +38,19 @@ protected:
     std::default_random_engine randomGenerator; /// !< The RNG to use to draw from the distribution 
     std::normal_distribution<FLOAT> distribution; /// !< The distribution to be used when proposing new lattice configurations
 
+    /// @brief Get the action for any two (neighbouring) sites on the lattice
+    /// @param s1 The vector of values at the first site
+    /// @param s2 The vector of values at the second site
+    /// @return The action for the connection of the two sites
+    virtual FLOAT getAction(std::vector<FLOAT> s1, std::vector<FLOAT> s2) = 0;
+
+    /// @brief Get the action for a particular lattice configuration
+    /// @param latticeToCheck The lattice to get the action for 
+    /// @return The action
+    FLOAT getAction(Lattice2D<FLOAT> &latticeToCheck);
+
+    bool periodicBC = false; /// <! whether to use periodic boundary conditions
+    /// \todo make boundary condition options some kind of enum or something
 };
 
 
@@ -46,21 +65,8 @@ public:
 
     void proposeLattice(FLOAT stepSize);
 
-    inline FLOAT getCurrentAction() { return getMagneticAction(lattice); }
-    inline FLOAT getProposedAction() { return getMagneticAction(proposedLattice); }
-
-    /// @brief Set whether or not to use periodic boundary conditions
-    /// @param newVal The new value
-    inline void setPeriodicBC(bool newVal = true) { periodicBC = newVal; }
-
-    /// @brief set the current lattice values to the proposed ones
-    inline void acceptProposedLattice() { lattice.set(proposedLattice); }
-
 private:
-    FLOAT getMagneticAction(Lattice2D<FLOAT> &latticeToCheck);
-    FLOAT getMagneticAction(std::vector<FLOAT> s1, std::vector<FLOAT> s2);
-    FLOAT magneticCoupling; /// <! The strength of the magnetic coupling between neighbouring lattice sites
+    FLOAT getAction(std::vector<FLOAT> s1, std::vector<FLOAT> s2);
 
-    bool periodicBC = false; /// <! whether to use periodic boundary conditions
-    /// \todo make boundary condition options some kind of enum or something
+    FLOAT magneticCoupling; /// <! The strength of the magnetic coupling between neighbouring lattice sites
 };
