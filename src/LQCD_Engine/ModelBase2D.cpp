4e739383ba135568b39443c59151329cca7678c7
Ewan Miller <ewan.miller@kcl.ac.uk>
2025-12-28 11:42:18 +0000

    fix periodic BC calculation bug in ModelBase2D and tidy

--- src/LQCD_Engine/ModelBase2D.cpp
+++ src/LQCD_Engine/ModelBase2D.cpp
@@ -4,42 +4,47 @@ FLOAT ModelBase2D::getAction(Lattice2D<FLOAT> &latticeToCheck) {
 
     FLOAT accum = 0.0;
 
+    const uint &sizeX = latticeToCheck.getSizeX();
+    const uint &sizeY = latticeToCheck.getSizeY();
+
     #pragma omp parallel for reduction(+:accum)
-    for (uint x = 0; x < latticeToCheck.getSizeX(); x ++){
+    for (uint x = 0; x < sizeX; x ++){
         
         FLOAT accum_thread = 0.0;
 
-        for (uint y = 0; y < latticeToCheck.getSizeY(); y ++){
+        for (uint y = 0; y < sizeY; y ++){
+
+            const auto &centralSite = latticeToCheck.getSite(x, y);
 
             if (x == 0) {
                 if (periodicBC) {
-                    accum_thread += getAction(latticeToCheck.getSite(x, 0), latticeToCheck.getSite(x, latticeToCheck.getSizeY() -1));
+                    accum_thread += getAction(centralSite, latticeToCheck.getSite(sizeX - 1, y));
                 }
             }
             else {
-                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x - 1, y));
+                accum_thread += getAction(centralSite, latticeToCheck.getSite(x - 1, y));
             }
             if (y == 0) {
                 if (periodicBC) {
-                    accum_thread += getAction(latticeToCheck.getSite(0, y), latticeToCheck.getSite(latticeToCheck.getSizeX() -1, y));
+                    accum_thread += getAction(centralSite, latticeToCheck.getSite(x, sizeY - 1));
                 }
             }
             else {
-                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x, y - 1));
+                accum_thread += getAction(centralSite, latticeToCheck.getSite(x, y - 1));
             }
             if (x == latticeToCheck.getSizeX() -1) {
                 if (periodicBC) {
-                    accum_thread += getAction(latticeToCheck.getSite(x, latticeToCheck.getSizeY() -1), latticeToCheck.getSite(x, 0));
+                    accum_thread += getAction(centralSite, latticeToCheck.getSite(0, y));
                 }
             }
             else {
-                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x + 1, y));
+                accum_thread += getAction(centralSite, latticeToCheck.getSite(x + 1, y));
             }
             if (y == latticeToCheck.getSizeY() -1) {
-                accum_thread += getAction(latticeToCheck.getSite(latticeToCheck.getSizeX() -1, y), latticeToCheck.getSite(0, y));
+                accum_thread += getAction(centralSite, latticeToCheck.getSite(x, 0));
             }
             else {
-                accum_thread += getAction(latticeToCheck.getSite(x, y), latticeToCheck.getSite(x, y + 1));
+                accum_thread += getAction(centralSite, latticeToCheck.getSite(x, y + 1));
             }
         }
 
